6e5beb5788666b9c2584afa6449b0266b930f02c
winnie <winniexx.0918@gmail.com>
2021-06-11 20:50:54 +0800

    新增三位數的阿姆斯壯數

--- armstrong/all.js
+++ armstrong/all.js
@@ -13,7 +13,7 @@ function fun() {
         alert('請輸入範圍')
     }
 
-    document.getElementById('outVal').innerHTML = narcissistic(low, high)
+    document.getElementById('outVal').innerHTML = narcissisticScope(low, high)
 }
 
 function isNarcissistic(number) {
@@ -31,7 +31,7 @@ function isNarcissistic(number) {
     return sum === number
 }
 
-function narcissistic(low, high) {
+function narcissisticScope(low, high) {
     let armstrongs = [];
     // let bound = Math.pow(5, n <= 10 ? n : 9)
     for (let i = low; i < high; i++) {
@@ -42,4 +42,35 @@ function narcissistic(low, high) {
     return armstrongs;
 }
 
-console.log(narcissistic(1, 1000));
+console.log(narcissisticScope(1, 1000));
+
+
+//找出三位數的阿姆斯壯數
+
+
+function isNarcissistic_a(number) {
+    let digits = [];
+    let num = number;
+    let sum = 0; //每個位數的次方之總和，最後會和 num 比較是否相同
+
+    while (num != 0) {
+        digits.push(num % 10);
+        num = parseInt(num / 10)
+    }
+    for (let i = 0; i < digits.length; i++) {
+        sum += Math.pow(digits[i], digits.length)
+    }
+    return sum === number
+}
+
+function narcissistic(n) {
+    let armstrongs = [];
+    let bound = Math.pow(10, n < 40 ? n : 39)
+    for (let i = 0; i < bound; i++) {
+        if (isNarcissistic_a(i)) {
+            armstrongs.push(i)
+        }
+    }
+    return armstrongs
+}
+document.getElementById('armstrong').innerHTML = narcissistic(3)
